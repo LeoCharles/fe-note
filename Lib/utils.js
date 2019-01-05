96dc0e6bb32ca71341a040ea0a6d11277cc3fba4
Leo <leofe200@163.com>
2019-01-05 17:42:31 +0800

    Update utils.js

--- Lib/utils.js
+++ Lib/utils.js
@@ -255,3 +255,55 @@ function $debounce(fn, delay = 100) {
       // fail
   })
  */
+ 
+ /**
+ * 微信实现 watch 属性，监听数据变化
+ * 首先在需要的页面引入
+ * 在 Page 的onLoad钩子设置监听器
+ */
+ function observe(obj, key, watchFun, deep, page) {
+  let val = obj[key];
+
+  if (val != null && typeof val === "object" && deep) {
+    Object.keys(val).forEach((item) => {
+      observe(val, item, watchFun, deep, page);
+    });
+  }
+
+  Object.defineProperty(obj, key, {
+    configurable: true,
+    enumerable: true,
+    set: function(value) {
+      watchFun.call(page, value, val);
+      val = value;
+
+      if (deep) {
+        observe(obj, key, watchFun, deep, page);
+      }
+    },
+    get: function() {
+      return val;
+    }
+  });
+}
+
+export function setWatcher(page) {
+  let data = page.data;
+  let watch = page.watch;
+
+  Object.keys(watch).forEach((item) => {
+    let targetData = data;
+    let keys = item.split(".");
+
+    for (let i = 0; i < keys.length - 1; i++) {
+      targetData = targetData[keys[i]];
+    }
+
+    let targetKey = keys[keys.length - 1];
+
+    let watchFun = watch[item].handler || watch[item];
+
+    let deep = watch[item].deep;
+    observe(targetData, targetKey, watchFun, deep, page);
+  });
+}
