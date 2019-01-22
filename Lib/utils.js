7d4e1d7199fcca039edee4559c3c8e957ca12171
Leo <leofe200@163.com>
2019-01-22 15:16:35 +0800

    Update utils.js

--- Lib/utils.js
+++ Lib/utils.js
@@ -35,6 +35,30 @@ function $deepClone(obj) {
   return temp
 }
 
+function deepCopy(data) {
+  const t = $typeOf(data)
+  let o
+  
+  if (t === 'array') {
+    o = []
+  } else if ( t === 'object') {
+    o = {}
+  } else {
+    return data
+  }
+
+  if (t === 'array') {
+    for (let i = 0; i < data.length; i++) {
+      o.push(deepCopy(data[i]))
+    }
+  } else if ( t === 'object') {
+    for (let i in data) {
+      o[i] = deepCopy(data[i])
+    }
+  }
+  return o
+}
+
 /**
  * 生成唯一ID
  */
