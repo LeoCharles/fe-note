fa817f36b9591151c5a4df232ab2ce015c751939
liucong <liucong@dodoca.com>
2019-04-16 14:59:05 +0800

    update

--- Tool/utils.js
+++ Tool/utils.js
@@ -146,6 +146,18 @@ function $parseTime(time, cFormat) {
   return timeStr
 }
 
+// 时间 格式化成 2018-12-12 12:12:00
+function $timestampToTime(timestamp) {
+	const date = new Date(timestamp);
+	const Y = date.getFullYear() + '-';
+	const M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+	const D = date.getDate() < 10 ? '0' + date.getDate() + ' ' : date.getDate() + ' ';
+	const h = date.getHours() < 10 ? '0' + date.getHours() + ':' : date.getHours() + ':';
+	const m = date.getMinutes() < 10 ? '0' + date.getMinutes() + ':' : date.getMinutes() + ':';
+	const s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+	return Y + M + D + h + m + s;
+}
+
 
 /**
  * 已过时间
